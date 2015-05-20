424edbd5822e23a5a8987191e9c8bed166430be6
John Dulin <dulinjd@gmail.com>
2015-05-20 13:44:22 -0400

    Added complex IO utilities, finished half_turn function, debugging quarter.

---
+++ complexIO.py
@@ -0,0 +1,41 @@
+"""
+John Dulin
+May 20, 2015
+Complex number IO utilities
+
+Some naive tools for writing complex numbers to file for cosmic topology research.
+"""
+
+import numpy as np
+
+def write_number(z, text):
+    """ Writes complex number out to specified open file object at current index.
+    z - Complex number numpy.complex128
+    text - Open text file with mode 'w'
+    """
+    u = np.real(z)
+    v = np.imag(z)
+    raw = str(u) + '+' + str(v) + 'i'
+    text.write(raw)
+
+def read_number(text):
+    """ Reads complex number from specified open file object at current index. 
+    text - Open text file with mode 'r'
+    """
+    buff = list()
+    for i in xrange(24):
+        char = text.read(1)
+        if char != 'i':
+            buff.append(char)
+        else:
+            break
+    uvpair = ''.join(buff).split('+')
+    return np.complex128(np.float64(uvpair[0]) + 1j * np.float64(uvpair[1]))
+
+def from_text(ring):
+    """ Converts a string complex to numpy.complex128. """
+    uvpair = ring.split('+')
+    uvpair[1] = uvpair[1].strip('j').strip('i')
+    return np.complex128(np.float64(uvpair[0]) + 1j * np.float64(uvpair[1]))
+   
+##  if __name__ == '__main__':
--- eigenmodes.py
+++ eigenmodes.py
@@ -1,7 +1,7 @@
 """
 John Dulin
 Case Western Reserve University
-April 27, 2015
+May 14, 2015
 """
 
 import sys
@@ -9,19 +9,66 @@ import argparse
 import numpy as np
 import csv
 
+##  Topology plane wave term functions.  Indexed by 'topologies' dict, produces a single plane wave indexed by j.  
+##  Factor of 2pi removed from wave vector.  Must be added in matrix computation.  
+##  Format of each file line is: [n, j, xi_nj, kx, ky, kz]
 def three_torus(n, j, size, angles):  
     mode = np.array([n, n, n])
-    T = np.array([[0, 0, size[0]], 
+    T = np.array([ [0, 0, size[0]], 
             [size[1]*np.sin(angles[0]), 0, size[1]], 
-                [size[2]*np.cos(angles[2])*np.sin(angles[1]), size[2]*np.sin(angles[2])*np.sin(angles[1]), size[2]*np.cos(angles[1])]])
+                [size[2]*np.cos(angles[2])*np.sin(angles[1]), 
+                size[2]*np.sin(angles[2])*np.sin(angles[1]), 
+                size[2]*np.cos(angles[1])] ])
     k = np.dot(T, mode)
-    return np.append([n, j, 1], k)
+    coeff = 1
+    return np.append([n, j, coeff], k)
 
 def half_turn(n, j, size, angles):
-    print "Half"
+    T = np.array([ [0, 0, size[0]], 
+            [size[1]*np.sin(angles[0]), 0, size[1]], 
+                [size[2]*np.cos(angles[2])*np.sin(angles[1]), 
+                size[2]*np.sin(angles[2])*np.sin(angles[1]), 
+                size[2]*np.cos(angles[1])] ])
+    if j == 1:
+        mode = np.array([n, n, n])
+        coeff = 1/np.sqrt(2)
+    elif j == 2:
+        mode = np.array([-n, -n, n])
+        coeff = 1/np.sqrt(2) * -1**mode[2]
+    
+    k = np.dot(T, mode)
+    return np.append([n, j, coeff], k)
 
 def quarter_turn(n, j, size, angles):
-    print "quarter"
+    mode = np.array([n, n, n])
+    T = np.array([ [0, 0, size[0]], 
+            [size[1]*np.sin(angles[0]), 0, size[1]], 
+                [size[2]*np.cos(angles[2])*np.sin(angles[1]), 
+                size[2]*np.sin(angles[2])*np.sin(angles[1]), 
+                size[2]*np.cos(angles[1])] ])
+    R = np.array([ [0, -1, 0],
+                    [1, 0, 0],
+                    [0, 0, 1] ])
+    x = 1/2.0
+    nz = mode[2]
+    ## TODO: Numpy arrays must be homogenous - Line 70 returns a complex128 array.
+    ## TODO: Complex output form for coeffs - '(1+0j)'.  Parens added.
+    if j >= 1:
+        coeff = x
+        if j >= 2:
+            coeff = np.complex128(x * j ** nz)
+            print 'test 2'
+            print coeff
+            if j >= 3:
+                coeff = np.complex128(x * j ** (2*nz))
+                if j == 4:
+                    coeff = np.complex128(x * j ** (3*nz))
+
+    k = np.dot(T, mode)  ## Translate
+    k = np.dot(R, k)     ## Rotate 
+    ##  T and R as defined do not commute because T includes the Lx, Ly, Lz dependencies of k.  So, we multiply k by 
+    ##  R after T to include the length dependencies in k before twisting.
+    return np.append([n, j, coeff], k)
 
 def third_turn():
     print "third turn"
@@ -33,12 +80,12 @@ topologies = {1 : three_torus,
               2 : half_turn, 
               3 : quarter_turn,
               4 : third_turn, 
-              5 : sixth_turn }
+              5 : sixth_turn}
 
 ## TODO: right number of b modes?
 bterms = {1:1, 2:2, 3:4, 4:3, 5:6}
 
-def test_eigenmodes(args):
+def eigenmodes(args):
     filename = '%(eigenmodes)s_Top%(top)s_%(lx)s_%(ly)s_%(lz)s_%(ax)s_%(ay)s_%(az)s.csv' % args
     size = np.array([args['lx'], args['ly'], args['lz']])
     angles = np.array([args['ax'], args['ay'], args['az']])
@@ -48,11 +95,10 @@ def test_eigenmodes(args):
     
     with open(filename, 'wb') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ')
-        for mode in xrange(1, modes+1):
-            for term in xrange(1, terms+1):
-                planewave = topologies[top](mode, term, size, angles).tolist()
-                n, j = planewave[0], planewave[1]
-                planewave[0], planewave[1] = int(n), int(j)
+        for n in xrange(1, modes+1):   ## Follow math convention - Index terms beginning with 1.
+            for j in xrange(1, terms+1):
+                planewave = topologies[top](n, j, size, angles).tolist()
+                planewave[0], planewave[1] = n, j
                 writer.writerow(planewave)
         
 def commands():
@@ -73,15 +119,16 @@ def commands():
     parser.add_argument('lz', metavar='Lz', type=np.float64, 
                         help='Length of the topology in the z dimension.')
     parser.add_argument('ax', metavar='Ax', type=np.float64, 
-                        help='Angle of shape obliqueness in the x direction.')
+                        help='Angle of shape obliqueness in the x direction in radians.')
     parser.add_argument('ay', metavar='Ay', type=np.float64, 
-                        help='Angle of shape obliqueness in the y direction.')
+                        help='Angle of shape obliqueness in the y direction in radians.')
     parser.add_argument('az', metavar='Az', type=np.float64, 
-                        help='Angle of shape obliqueness in the z direction.')
+                        help='Angle of shape obliqueness in the z direction in radians.')
 
     return vars(parser.parse_args())
 
 if __name__ == '__main__':
     """  Run application. """
     print "Test in Progress"
-    test_eigenmodes(commands())
+    eigenmodes(commands())
+    
